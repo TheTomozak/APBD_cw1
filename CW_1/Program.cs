c0a3d668a256959a6ae673fdba2882d4b9d6f470
s18969 <s18969@pjwstk.edu.pl>
2020-03-05 13:08:46 +0100

    Druga

--- CW_1/Program.cs
+++ CW_1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Net.Http;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace CW_1
@@ -8,10 +9,27 @@ namespace CW_1
     {
         public static async Task Main(string[] args)
         {
+
+
+
             // Console.WriteLine("Hello World!");
 
             var client = new HttpClient();
             var result =await client.GetAsync("https://www.pja.edu.pl");
+
+
+            if (!result.IsSuccessStatusCode) return;
+
+            string html = await result.Content.ReadAsStringAsync();
+            var regex = new Regex("[a-z]+[a-z0-9]*@[a-z.]+", RegexOptions.IgnoreCase);
+
+            var mathches = regex.Matches(html);
+
+            foreach(var m in mathches)
+            {
+                Console.WriteLine(m);
+            }
+
 
             Console.WriteLine("Koniec!");
 
